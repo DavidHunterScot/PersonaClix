5cf6cd3fffac9872afc0ba6ab18b8956b34b9229
DavidHunterScot <david.hunter@echoverse.cc>
2023-01-04 12:18:07 +0000

    Added favicon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   
   return {
     dir: {
