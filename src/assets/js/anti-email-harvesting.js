44d6750a3edd88adab1bb26bcd8d46b0d3560e59
DavidHunterScot <david.hunter@echoverse.cc>
2023-02-26 17:01:51 +0000

    Made changes to the contact page and email address harvesting protection

--- src/assets/js/anti-email-harvesting.js
+++ src/assets/js/anti-email-harvesting.js
@@ -6,16 +6,18 @@ for( let i = 0; i < contact_methods.length; i++ )
 
     if( email_addresses.length == 1 )
     {
-        var email_address = email_addresses[ 0 ].innerHTML.replaceAll( ' [at] ', '@' ).replaceAll( ' [dot] ', '.' );
-
-        email_addresses[ 0 ].innerHTML = email_address;
-
-        var email_links = contact_methods[ i ].getElementsByClassName( 'email_link' );
-
-        if( email_links.length == 1 )
+        if( email_addresses[ 0 ].hasAttribute( 'email-address' ) )
         {
-            email_links[ 0 ].href = "mailto:" + email_address;
-            email_links[ 0 ].target = "_blank";
+            var email_address = email_addresses[ 0 ].getAttribute( 'email-address' ).replaceAll( ' [at] ', '@' ).replaceAll( ' [dot] ', '.' );
+
+            email_addresses[ 0 ].innerHTML = "Email";
+            email_addresses[ 0 ].href = "mailto:" + email_address;
+            email_addresses[ 0 ].target = "_blank";
+        }
+        else
+        {
+            var email_address = email_addresses[ 0 ].innerHTML.replaceAll( ' [at] ', '@' ).replaceAll( ' [dot] ', '.' );
+            email_addresses[ 0 ].innerHTML = email_address;
         }
     }
 }
--- src/contact.php
+++ src/contact.php
@@ -16,17 +16,36 @@ $page_content = function()
 
         <section class="contact section w3-padding-64">
             <div class="container w3-auto w3-padding">
-                <div class="methods w3-row">
-                    <div class="method w3-quarter w3-white w3-round-large w3-card w3-padding-large">
-                        <div class="icon">
+                <div class="methods w3-row w3-white">
+                <div class="method w3-col m3 w3-padding-large w3-center">
+                        <div class="icon w3-bottombar w3-border-amber">
+                            <img src="<?php echo $icons_url; ?>/person-crop-square-fill.svg" alt="Person Crop Square Icon" class="w3-image">
+                        </div>
+
+                        <p>
+                            <div><b>General Enquiries</b></div>
+                            <div style="overflow-x: auto; white-space: nowrap;"><span class="email_address">contact [at] personaclix [dot] com</span></div>
+                        </p>
+                    </div>
+
+                    <div class="method w3-col m3 w3-padding-large w3-center">
+                        <div class="icon w3-bottombar w3-border-amber">
                             <img src="<?php echo $avatars_url; ?>/davidhunter_illustration_transparent_1.png" alt="David Hunter Illustration" class="w3-image">
                         </div>
 
-                        <div class="button">
-                            <a href="#" id="email_link" class="w3-button w3-amber w3-round-large w3-block email_link">David Hunter</a>
+                        <p><b>David Hunter</b><br>Service Operator</p>
+
+                        <p><a href="#" class="email_address" email-address="david [dot] hunter [at] personaclix [dot] com"></a> <a href="https://davidhunter.scot" target="_blank">Web</a></p>
+                    </div>
+
+                    <div class="method w3-col m3 w3-padding-large w3-center">
+                        <div class="icon w3-bottombar w3-border-amber">
+                            <img src="<?php echo $avatars_url; ?>/taramckale_personaclix_representitive.png" alt="Tara McKale Photo" class="w3-image">
                         </div>
 
-                        <p class="email_address w3-center" style="overflow-x: auto; white-space: nowrap;">david [dot] hunter [at] themailbox [dot] io</p>
+                        <p><b>Tara McKale</b><br>Promotional Mascot</p>
+
+                        <p><a href="#" class="email_address" email-address="tara [dot] mckale [at] personaclix [dot] com"></a> <a href="https://taramckale.mywebaddress.me" target="_blank">Web</a></p>
                     </div>
                 </div>
             </div>
